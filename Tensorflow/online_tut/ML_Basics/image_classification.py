60cfa315567f43133d3fb9ee249ac4c3e9e74e0c
Sherman <steam.hyperpolyglot@outlook.sg>
2021-08-27 19:19:23 +0800

    Added a few more examples from the book.

--- Tensorflow/online_tut/ML_Basics/image_classification.py
+++ Tensorflow/online_tut/ML_Basics/image_classification.py
@@ -5,4 +5,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trousers', 'Pullover', 'Dress', 'Coat', 'Sandal',
+    'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# print(train_images.shape)
+print(train_labels)
